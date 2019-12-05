6c165ee1710f1d7a5d8b1f259018f31f67335e8d
tftitems <tftitems@gmail.com>
2019-12-05 15:09:39 -0500

    Updates

---
+++ precache-manifest.15fc2d7207f2523301a2080a7df91cbe.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fd4deba7add10de2b5aefacc5a8560d",
+    "url": "/tftitems/index.html"
+  },
+  {
+    "revision": "8b849ac8a5e42c7b2f64",
+    "url": "/tftitems/static/css/main.dc78eb17.chunk.css"
+  },
+  {
+    "revision": "4bf9072a6621f727eeb6",
+    "url": "/tftitems/static/js/2.d42e716b.chunk.js"
+  },
+  {
+    "revision": "8b849ac8a5e42c7b2f64",
+    "url": "/tftitems/static/js/main.e7f4e3b4.chunk.js"
+  },
+  {
+    "revision": "2ec2b06605843f57e1c9",
+    "url": "/tftitems/static/js/runtime-main.c9d0bc91.js"
+  },
+  {
+    "revision": "9d846b1ccf2a934cacc93f434b4e4c8c",
+    "url": "/tftitems/static/media/elements.9d846b1c.jpeg"
+  },
+  {
+    "revision": "012575dda35a0fe91801e0b76edbdf79",
+    "url": "/tftitems/static/media/golden-spatula.012575dd.png"
+  },
+  {
+    "revision": "eb7b6abc196814335e8dbf9e6f68b7fe",
+    "url": "/tftitems/static/media/hauntlingbackground.eb7b6abc.jpeg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/tftitems/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "09da0e43aead78ba05491083d87181cc",
+    "url": "/tftitems/static/media/penguinsword.09da0e43.jpeg"
+  },
+  {
+    "revision": "e8f69e0e9acceb5ed78053cd3acd41cc",
+    "url": "/tftitems/static/media/talisman-of-light.e8f69e0e.png"
+  }
+]);
